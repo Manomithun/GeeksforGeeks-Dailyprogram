25f4c312c4daff8668e489868bf1c86c5d16e750
Manomithun <manomarutharasu@gmail.com>
2025-01-06 22:17:27 +0530

    added

---
+++ PrintAnagramsTogether.java
@@ -0,0 +1,58 @@
+/**
+Print Anagrams Together
+Difficulty: MediumAccuracy: 65.78%Submissions: 92K+Points: 4
+Given an array of strings, return all groups of strings that are anagrams. The strings in each group must be arranged in the order of their appearance in the original array. Refer to the sample case for clarification.
+
+Examples:
+
+Input: arr[] = ["act", "god", "cat", "dog", "tac"]
+Output: [["act", "cat", "tac"], ["god", "dog"]]
+Explanation: There are 2 groups of anagrams "god", "dog" make group 1. "act", "cat", "tac" make group 2.
+Input: arr[] = ["no", "on", "is"]
+Output: [["is"], ["no", "on"]]
+Explanation: There are 2 groups of anagrams "is" makes group 1. "no", "on" make group 2.
+**/
+
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+    }
+}
